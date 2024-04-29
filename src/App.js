99741bca3a3954d0d1140afa881397b238720f4a
John Doe <johndoe@email.com>
2024-04-29 21:46:02 +0530

    support grps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
 import logo from './logo.svg';
 import './App.css';
+import blue from './blue.png';
+import { useState } from 'react';
+
 
 function App() {
+
+const [modal, setmodal] = useState(false);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>BMS BLOGS
+
+      <button onClick={()=>setmodal(!modal)}> submit a blog</button>
+      
+      
+      </div>
+      
+
+      <div className='blog_body'>
+            <div className='blog_div'>
+                {/* <div className='blog_thumbnail'>
+                </div> */}
+            </div>
+            <div className='blog_div'>
+                {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>
+                 {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>
+               {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>  
+            {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+            <div className='blog_div'>
+               {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>
+                {/* <div className='blog_thumbnail'>
+                </div> */}
+            </div>
+            <div className='blog_div'>
+                {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>
+                 {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>
+               {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+
+            <div className='blog_div'>  
+            {/* <div className='blog_thumbnail'>
+                </div> */}
+
+            </div>
+            <div className='blog_div'>
+               
+
+            </div>
+
+            <div className='blog_div'>
+               
+
+            </div>
+
+            <div className='blog_div'>
+               
+
+            </div>
+
+            <div className='blog_div'>
+               
+
+            </div>
+            
+      </div>
+
+     { modal? <div className='modal_container'>
+
+          <div className='modal'>
+
+            
+            <div>
+              <button 
+              onClick={()=>setmodal(!modal)}>X
+              </button>
+            </div>
+
+            <div>
+              < div> keep it anonymous? </div>
+             
+              <div>
+                <label class="switch">
+                    <input type="checkbox" ></input>
+                      <span class="slider round"></span>
+                </label>
+              </div>   
+            </div>
+
+            <div><input type='text' placeholder='name'></input></div>
+            <div><input type='text' placeholder='your entry'></input></div>
+
+            <div><button>submit</button></div>
+
+         </div>
+      </div> :<></>}
+
     </div>
   );
 }
